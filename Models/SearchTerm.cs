726085d6d447aa092b10b064cff17bf37b099366
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-11 15:18:32 -0500

    defined properties of Search Term object

--- Models/SearchTerm.cs
+++ Models/SearchTerm.cs
@@ -2,5 +2,24 @@ namespace ledger.Models
 {
     public class SearchTerm
     {
+        // HINT: a search term should contain field names
+
+        /// <summary>
+        /// Search clause used in query as a string
+        /// </summary>
+        /// <value></value>
+        public string Clause { get; set; }
+
+        /// <summary>
+        /// Selection modifier as AND or OR 
+        /// </summary>
+        /// <value></value>
+        public string SelectionModifier { get; set; }
+
+        /// <summary>
+        /// Order of clause 
+        /// </summary>
+        /// <value></value>
+        public int Order { get; set; }
     }
 }
